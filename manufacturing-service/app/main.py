f0eea458fc00a2baf53adf81d2d738abca5d5e7a
adastra-lt <153209606+adastra-lt@users.noreply.github.com>
2023-12-16 23:18:52 +0200

    Separating API namespaces

--- manufacturing-service/app/main.py
+++ manufacturing-service/app/main.py
@@ -17,6 +17,6 @@ async def shutdown():
     await database.disconnect()
 
 # Include your routers here
-app.include_router(products, prefix='/api/v1/manufacturing', tags=['products'])
-app.include_router(sales, prefix='/api/v1/manufacturing', tags=['sales'])
-app.include_router(inventory, prefix='/api/v1/manufacturing', tags=['inventory'])
+app.include_router(products, prefix='/api/v1/products', tags=['products'])
+app.include_router(sales, prefix='/api/v1/sales', tags=['sales'])
+app.include_router(inventory, prefix='/api/v1/inventory', tags=['inventory'])
